e7c105cede9bed3bcd7b043b248623a202e10253
Michele Miglio <M.Miglio@basedigitalegroup.com>
2026-06-17 10:11:26 +0200

    Prima vista Anagrafica Clienti

---
+++ CLab/ViewModels/ClientiViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using CLab.Data;
+using CLab.Models;
+
+namespace CLab.ViewModels
+{
+    public class ClientiViewModel : ViewModelBase
+    {
+        public ObservableCollection<Cliente> Clienti { get; set; }
+
+        public ClientiViewModel()
+        {
+            Clienti = new ObservableCollection<Cliente>();
+            CaricaClienti();
+        }
+
+        private void CaricaClienti()
+        {
+            using (var db = new ClabDbContext())
+            {
+                var listaClienti = db.Clienti.ToList();
+
+                Clienti.Clear();
+                foreach (var cliente in listaClienti)
+                {
+                    Clienti.Add(cliente);
+                }
+            }
+        }
+    }
+}
